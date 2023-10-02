6f72c2d7653f4977429e9ac3b7c0ee61360aba11
Yann Papouin <yann.papouin@gmail.com>
2023-10-02 08:55:05 +0200

    OnError argument added

--- ac_status.py
+++ ac_status.py
@@ -16,29 +16,37 @@ def dict_diff(first_dict, second_dict):
     return value
 
 
-def check_ac_status(db_path, db_filename):
+def check_ac_status(db_path, db_filename, on_error_cmd):
     database = os.path.join(db_path, db_filename)
     if os.path.exists(database):
         con = sqlite3.connect(database)
 
         cur = con.cursor()
-        cur.execute("SELECT object, value FROM status")
+        try:
+            cur.execute("SELECT object, value FROM status")
+            vals = dict(cur.fetchall())
+            cur.close()
+            if DEBUG:
+                output_filename = "vals_" + datetime.now().strftime("%H_%M_%S") + ".json"
+                # Print current values to console
+                for key, value in vals.items():
+                    print(key, value)
+                # Write same values to a file for diff
+                with open(output_filename, "w") as fp:
+                    json.dump(vals, fp, indent=4)
 
-        vals = dict(cur.fetchall())
-        if DEBUG:
-            output_filename = "vals_" + datetime.now().strftime("%H_%M_%S") + ".json"
-            # Print current values to console
-            for key, value in vals.items():
-                print(key, value)
-            # Write same values to a file for diff
-            with open(output_filename, "w") as fp:
-                json.dump(vals, fp, indent=4)
-
-        if vals["UPS.PowerSummary.PresentStatus.ACPresent"] == "1":
-            res = 0
-        else:
-            res = 1
-
+            if vals["UPS.PowerSummary.PresentStatus.ACPresent"] == "1":
+                res = 0
+            else:
+                res = 1
+        except sqlite3.OperationalError as e:
+            print(e.args[0])
+            if e.args[0] == "database is locked":
+                res = -1
+                if on_error_cmd:
+                    print("Run", on_error_cmd)
+                    os.system(on_error_cmd)
+                
         con.close()
     else:
         print(database, "not found")
@@ -60,7 +68,12 @@ if __name__ == "__main__":
         help="Database filename (eg: mc2.db)",
         default="mc2.db",
     )
+    parser.add_argument(
+        "--on-error-cmd",
+        metavar=("ERROR_CMD"),
+        help="docker restart ipp",
+    )
     args = parser.parse_args(sys.argv[1:])
 
-    return_code = check_ac_status(args.db_path, args.db_filename)
+    return_code = check_ac_status(args.db_path, args.db_filename, args.on_error_cmd)
     sys.exit(return_code)
